1537cad0e3ccaa3d9870d9a17c298a5a577224f3
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2022-03-20 21:00:26 +0300

    add authentication decorator

--- app.ts
+++ app.ts
@@ -13,6 +13,25 @@ class APIroutes {
         }
     }
 
+
+    @logRoute()
+    @route('get', '/people')
+    @authenticate('123456')
+    public peopleRoute(req: Request, res: Response) {
+        return {
+            people: [
+                {
+                    "firstName": "David",
+                    "lastName": "Tucker"
+                },
+                {
+                    "firstName:": "Sammy",
+                    "lastName": "Davis"
+                }
+            ]
+        }
+    }
+
 }
 
 function route(method: string, path: string): MethodDecorator {
@@ -31,8 +50,27 @@ function logRoute(): MethodDecorator {
         const original = descriptor.value;
         descriptor.value = function (...args: any[]) {
             let req = args[0] as Request;
-            console.log(`${req?.url} and ${req?.method} and ${req?.ip} and ${req?.originalUrl}  called`);
+            console.log(`${req?.url} and ${req?.method} called`);
             return original.apply(this, args);
+        };
+
+    };
+};
+
+function authenticate(key: string): MethodDecorator {
+
+    return function (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor): void {
+        const original = descriptor.value;
+        descriptor.value = function (...args: any[]) {
+            const req = args[0] as Request;
+            const res = args[1] as Response;
+            const headers = req.headers;
+
+            if(headers.hasOwnProperty('apikey') && headers.apikey == key ) {
+                return original.apply(this, args)
+            }
+            
+            res.status(403).json({ error: "Not Authorized"})
         };
 
     };
